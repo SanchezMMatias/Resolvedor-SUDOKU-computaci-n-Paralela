6a40fb78911d3e6d82286726668bae0d051a4b03
Matías Sánchez Muñoz <sanchezm.matias@gmail.com>
2024-10-25 12:40:13 -0300

    Update Node.hpp
    comentarios

--- ArchivosHPP/Node.hpp
+++ ArchivosHPP/Node.hpp
@@ -1,49 +1,41 @@
 #ifndef NODE_HPP
 #define NODE_HPP
 
-
 #include <string>
 
-
+// Forward declaration de la clase ColumnNode
 class ColumnNode;
 
-// A DancingNode object is used to model a node in the quadruple-chained list for exact cover problem.
-class DancingNode
-{
+// Un objeto DancingNode se utiliza para modelar un nodo en la lista cuádruplemente enlazada para el problema de cobertura exacta.
+class DancingNode {
 public:
-    DancingNode* left;
-    DancingNode* right;
-    DancingNode* top;
-    DancingNode* bottom;
-    ColumnNode* column;
+    DancingNode* left;   // Puntero al nodo de la izquierda
+    DancingNode* right;  // Puntero al nodo de la derecha
+    DancingNode* top;    // Puntero al nodo de arriba
+    DancingNode* bottom; // Puntero al nodo de abajo
+    ColumnNode* column;  // Puntero al nodo de la columna correspondiente
 
 public:
-    DancingNode();
-    DancingNode(ColumnNode* c);
-
-    DancingNode* linkDown(DancingNode* node);
-    DancingNode* linkRight(DancingNode* node);
-
-    void removeLeftRight();
-    void reinsertLeftRight();
-
-    void removeTopBottom();
-    void reinsertTopBottom();
+    DancingNode();                    // Constructor por defecto
+    DancingNode(ColumnNode* c);       // Constructor que inicializa con un nodo columna
+    DancingNode* linkDown(DancingNode* node);   // Enlaza el nodo dado hacia abajo
+    DancingNode* linkRight(DancingNode* node);  // Enlaza el nodo dado hacia la derecha
+    void removeLeftRight();            // Elimina este nodo de la lista izquierda-derecha
+    void reinsertLeftRight();          // Reinsertar este nodo en la lista izquierda-derecha
+    void removeTopBottom();            // Elimina este nodo de la lista arriba-abajo
+    void reinsertTopBottom();          // Reinsertar este nodo en la lista arriba-abajo
 };
 
-// A ColumnNode object is used to link the current node to the belonging column in the cover matrix.
-class ColumnNode : public DancingNode
-{
+// Un objeto ColumnNode se utiliza para enlazar el nodo actual a la columna correspondiente en la matriz de cobertura.
+class ColumnNode : public DancingNode {
 public:
-    int size;
-    std::string name;
+    int size;           // Tamaño de la columna (número de nodos en la columna)
+    std::string name;   // Nombre de la columna
 
 public:
-    ColumnNode(std::string n);
-
-    void cover();
-    void uncover();
+    ColumnNode(std::string n);   // Constructor que inicializa con un nombre de columna
+    void cover();                // Cubrir esta columna
+    void uncover();              // Descubrir esta columna
 };
 
-
-#endif   // NODE_HPP
+#endif  // NODE_HPP
