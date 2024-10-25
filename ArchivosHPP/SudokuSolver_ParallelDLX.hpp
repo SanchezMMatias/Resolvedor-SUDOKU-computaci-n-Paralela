bf4fbc956215db13032ba966e14d9343e9bd1ed2
Matías Sánchez Muñoz <sanchezm.matias@gmail.com>
2024-10-25 12:48:31 -0300

    Update SudokuSolver_ParallelDLX.hpp
    comentarios

--- ArchivosHPP/SudokuSolver_ParallelDLX.hpp
+++ ArchivosHPP/SudokuSolver_ParallelDLX.hpp
@@ -1,39 +1,38 @@
 #ifndef SUDOKUSOLVER_PARALLELDANCINGLINKS_HPP
 #define SUDOKUSOLVER_PARALLELDANCINGLINKS_HPP
 
+#include "SudokuBoard.hpp"   
+#include "SudokuSolver.hpp"  
+#include "Node.hpp"          
+#include <vector>           
 
-#include "SudokuBoard.hpp"
-#include "SudokuSolver.hpp"
-#include "Node.hpp"
-#include <vector>
-
-
-// DLX: apply Dancing Links techniques and Algorithm X to solve the exact cover problem.
-// Implementation of DLX algorithm written in C++ is based on the following Java tutorial:
-// https://medium.com/javarevisited/building-a-sudoku-solver-in-java-with-dancing-links-180274b0b6c1
-class SudokuSolver_ParallelDLX : public SudokuSolver
-{
+class SudokuSolver_ParallelDLX : public SudokuSolver {
 private:
-	SudokuBoard _originalBoard;
-	CoverMatrix _coverMatrix;
-    ColumnNode* _header;
-    std::vector<DancingNode*> _answer;
-	int _numberOfColumns;   // _BOARD_SIZE * _BOARD_SIZE * _MAX_VALUE
-	int _numberOfRows;      // _BOARD_SIZE * _BOARD_SIZE * _NUM_CONSTRAINTS
+    SudokuBoard _originalBoard;       // Tablero original de Sudoku
+    CoverMatrix _coverMatrix;         // Matriz de cobertura para el Sudoku
+    ColumnNode* _header;              // Nodo cabecera de la lista de DLX
+    std::vector<DancingNode*> _answer; // Vector de nodos que conforman la solución
+    int _numberOfColumns;             // _BOARD_SIZE * _BOARD_SIZE * _MAX_VALUE
+    int _numberOfRows;                // _BOARD_SIZE * _BOARD_SIZE * _NUM_CONSTRAINTS
 
 public:
+    // Constructor que inicializa el solucionador de Sudoku paralelo con Dancing Links
     SudokuSolver_ParallelDLX(SudokuBoard& board, bool print_message=true);
 
-	ColumnNode* createDLXList(CoverMatrix& coverMatrix);
+    // Crea la lista de DLX a partir de la matriz de cobertura
+    ColumnNode* createDLXList(CoverMatrix& coverMatrix);
 
-	SudokuBoard convertToSudokuGrid(std::vector<DancingNode*> answer);
+    // Convierte la solución en forma de nodos de DLX a un tablero de Sudoku
+    SudokuBoard convertToSudokuGrid(std::vector<DancingNode*> answer);
 
-	// Solves the given Sudoku board using parallel dancing links algorithm
+    // Resuelve el tablero de Sudoku dado usando el algoritmo de Dancing Links en paralelo
     virtual void solve() override { solve_kernel(0); }
+
+    // Función de kernel para resolver el Sudoku
     void solve_kernel(int k);
 
-	ColumnNode* selectColumnNodeHeuristic(ColumnNode* c, int k);
+    // Selecciona el nodo de columna usando una heurística
+    ColumnNode* selectColumnNodeHeuristic(ColumnNode* c, int k);
 };
 
-
-#endif   // SUDOKUSOLVER_PARALLELDANCINGLINKS_HPP
+#endif // SUDOKUSOLVER_PARALLELDANCINGLINKS_HPP
