a1102dffdcd380c1a2b169eeb9ff042350536357
Matías Sánchez Muñoz <sanchezm.matias@gmail.com>
2024-10-25 12:47:01 -0300

    Update SudokuSolver_ParallelBruteForce.hpp
    comentarios

--- ArchivosHPP/SudokuSolver_ParallelBruteForce.hpp
+++ ArchivosHPP/SudokuSolver_ParallelBruteForce.hpp
@@ -1,38 +1,35 @@
 #ifndef SUDOKUSOLVER_PARALLELBRUTEFORCE_HPP
 #define SUDOKUSOLVER_PARALLELBRUTEFORCE_HPP
 
+#include "SudokuBoard.hpp"     
+#include "SudokuSolver.hpp"       
+#include "SudokuBoardDeque.hpp"  
 
-#include "SudokuBoard.hpp"
-#include "SudokuSolver.hpp"
-#include "SudokuBoardDeque.hpp"
-
-
-class SudokuSolver_ParallelBruteForce : public SudokuSolver
-{
+// Clase SudokuSolver_ParallelBruteForce que hereda de SudokuSolver
+class SudokuSolver_ParallelBruteForce : public SudokuSolver {
 private:
-	SudokuBoardDeque _board_deque;
+    SudokuBoardDeque _board_deque;   // Deque para almacenar los tableros de Sudoku
 
 public:
-	SudokuSolver_ParallelBruteForce(SudokuBoard& board, bool print_message=true);
+    // Constructor que inicializa el solucionador de Sudoku paralelo de fuerza bruta
+    SudokuSolver_ParallelBruteForce(SudokuBoard& board, bool print_message=true);
 
-    // Divides one Sudoku problem into several simpler sub-problems and push them to the end of board deque
-	void bootstrap();
-	void bootstrap(SudokuBoardDeque& boardDeque, int indexOfRows);
+    // Divide un problema de Sudoku en varios subproblemas más simples y los empuja al final del deque de tableros
+    void bootstrap();
+    void bootstrap(SudokuBoardDeque& boardDeque, int indexOfRows);
 
-	// Solves the given Sudoku board using parallel brute force algorithm
-	virtual void solve() override
-	{
-		/* Choose one of the following kernels to execute */
-		solve_kernel_1();
-		// solve_kernel_2();
-		// solve_bruteforce_par(_board, 0, 0);
-	}
-	
-	void solve_kernel_1();
-	void solve_kernel_2();
-	void solve_bruteforce_seq(SudokuBoard& board, int row, int col);
-	void solve_bruteforce_par(SudokuBoard& board, int row, int col);
+    // Resuelve el tablero de Sudoku dado usando el algoritmo de fuerza bruta paralela
+    virtual void solve() override {
+        /* Elige uno de los siguientes kernels para ejecutar */
+        solve_kernel_1();
+        // solve_kernel_2();
+        // solve_bruteforce_par(_board, 0, 0);
+    }
+
+    void solve_kernel_1();  // Definición de kernel de resolución 1
+    void solve_kernel_2();  // Definición de kernel de resolución 2
+    void solve_bruteforce_seq(SudokuBoard& board, int row, int col);  // Resolución secuencial de fuerza bruta
+    void solve_bruteforce_par(SudokuBoard& board, int row, int col);  // Resolución paralela de fuerza bruta
 };
 
-
-#endif   // SUDOKUSOLVER_PARALLELBRUTEFORCE_HPP
+#endif // SUDOKUSOLVER_PARALLELBRUTEFORCE_HPP
